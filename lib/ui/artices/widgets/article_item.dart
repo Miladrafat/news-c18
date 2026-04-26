25c35604e45566ee3f4474266c198d5fad63ced2
miladsulhi76 <miladsulhi76@gmail.com>
2026-04-26 16:30:17 +0200

    -Add button sheet feature

--- lib/ui/artices/widgets/article_item.dart
+++ lib/ui/artices/widgets/article_item.dart
@@ -13,51 +13,134 @@ class ArticleItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: REdgeInsets.all(8),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16.r),
-        border: Border.all(color: ColorsManager.lightPrimaryColor),
-      ),
-      child: Column(
-        spacing: 10.h,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadiusGeometry.circular(8.r),
-            child: CachedNetworkImage(
-              imageUrl: article.urlToImage ?? "",
-              height: 220.h,
-              fit: BoxFit.cover,
-              placeholder: (context, url) =>
-                  Center(child: CircularProgressIndicator()),
-              errorWidget: (context, url, error) =>
-                  Center(child: Icon(Icons.error, size: 40.sp)),
+    return InkWell(
+      onTap:  () {
+        showMyBottomSheet(context);
+      },
+      child: Container(
+        padding: REdgeInsets.all(8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16.r),
+          border: Border.all(color: ColorsManager.lightPrimaryColor),
+        ),
+        child: Column(
+          spacing: 10.h,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadiusGeometry.circular(8.r),
+              child: CachedNetworkImage(
+                imageUrl: article.urlToImage ?? "",
+                height: 220.h,
+                fit: BoxFit.cover,
+                placeholder: (context, url) =>
+                    Center(child: CircularProgressIndicator()),
+                errorWidget: (context, url, error) =>
+                    Center(child: Icon(Icons.error, size: 40.sp)),
+              ),
             ),
-          ),
-          Text(
-            article.title ?? "",
-            style: Theme.of(
-              context,
-            ).textTheme.headlineMedium?.copyWith(fontSize: 16.sp),
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: Text(
-                  "By : ${article.author ?? ""}",
+            Text(
+              article.title ?? "",
+              style: Theme.of(
+                context,
+              ).textTheme.headlineMedium?.copyWith(fontSize: 16.sp),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    "By : ${article.author ?? ""}",
+                    style: Theme.of(context).textTheme.titleSmall,
+                  ),
+                ),
+                Text(
+                  timeago.format(DateTime.parse(article.publishedAt ?? "")),
                   style: Theme.of(context).textTheme.titleSmall,
                 ),
-              ),
-              Text(
-                timeago.format(DateTime.parse(article.publishedAt ?? "")),
-                style: Theme.of(context).textTheme.titleSmall,
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
-
+  void showMyBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      builder: (context) {
+        return Container(
+          padding: REdgeInsets.all(8),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16.r),
+            border: Border.all(color: ColorsManager.lightPrimaryColor),
+          ),
+          child: Column(
+            mainAxisSize: .min,
+            spacing: 10.h,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadiusGeometry.circular(8.r),
+                child: CachedNetworkImage(
+                  imageUrl: article.urlToImage ?? "",
+                  height: 220.h,
+                  fit: BoxFit.cover,
+                  placeholder: (context, url) =>
+                      Center(child: CircularProgressIndicator()),
+                  errorWidget: (context, url, error) =>
+                      Center(child: Icon(Icons.error, size: 40.sp)),
+                ),
+              ),
+              Text(
+                article.title ?? "",
+                style: Theme.of(
+                  context,
+                ).textTheme.headlineMedium?.copyWith(fontSize: 16.sp),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      "By : ${article.author ?? ""}",
+                      style: Theme.of(context).textTheme.titleSmall,
+                    ),
+                  ),
+                  Text(
+                    timeago.format(DateTime.parse(article.publishedAt ?? "")),
+                    style: Theme.of(context).textTheme.titleSmall,
+                  ),
+                ],
+              ),
+              Text(
+                article.description ?? "",
+                style: Theme.of(
+                  context,
+                ).textTheme.headlineMedium?.copyWith(fontSize: 16.sp),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    elevation: 2,
+                    padding: REdgeInsets.symmetric(vertical: 16.h),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.r),
+                    ),
+                  ),
+                  child: Text("View Full Articel",style: TextStyle(
+                      color: Theme.of(context).colorScheme.secondary
+                  ),),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
 
 }
